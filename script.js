8ecaa401a048f19480ed0c486bd2fa7d44a6df9d
Marco Wille <birdscur@outlook.dem>
2022-10-26 11:27:43 +0200

    end v1.0

--- script.js
+++ script.js
@@ -59,6 +59,26 @@ function draw()
     }
 }
 
+function changeThemeDark()
+{
+    getId('gameContainer').style.background ='linear-gradient(-45deg, #7d7c7c, #9a9697, #535b5e, #464a49)';
+    getId('gameContainer').style.backgroundSize=' 400% 400%'
+    getId('gameContainer').style.animation ='gradient 15s ease infinite';
+}
+function changeThemeYellowOrange()
+{
+   
+    getId('gameContainer').style.background ='linear-gradient(-45deg, #bead24, #adaa1a, #b07c13, #ab7524)';
+    getId('gameContainer').style.backgroundSize=' 400% 400%'
+    getId('gameContainer').style.animation ='gradient 15s ease infinite';
+}
+
+function changeThemeBlueRed()
+{
+    getId('gameContainer').style.background ='linear-gradient(-45deg, #ee7752, #5a1932, #166683, #23d5ab)';
+    getId('gameContainer').style.backgroundSize=' 400% 400%'
+    getId('gameContainer').style.animation ='gradient 15s ease infinite';
+}
 function checkForWin()
 {
     let winner;
@@ -117,5 +137,26 @@ function checkForWin()
             getId('gameOverScreen').classList.remove('hide')
         }, 2000)
     }
+
+  
     
+}
+
+function restart()
+{
+    fields =[];
+    gameOver = false;
+    getId('gameOverScreen').classList.add('hide')
+    getId('gameMenu2').classList.add('hide')
+    getId('gameField').classList.remove('hide')
+
+    for (let i = 0; i < 7; i++) {
+        getId('line' + i).style.transform ='scaleX(0.0)'
+        
+    }
+    for (let i = 0; i < 9; i++) {
+        getId('cross_' + i).classList.add('hide')
+        getId('circle_' + i).classList.add('hide')
+        
+    }
 }
